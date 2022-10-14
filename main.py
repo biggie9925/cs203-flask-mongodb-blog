400f0e2a2169233bce7101be3ef222cab120e309
elliehkim <84781000+elliehkim@users.noreply.github.com>
2022-10-14 14:52:39 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-    return render_template("single_post.html")
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
     app.run(debug=True)
