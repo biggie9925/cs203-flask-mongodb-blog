e21b11a78c33a6db86d032dafe0128315e052339
David Mackey-Ward <89782344+biggie9925@users.noreply.github.com>
2022-10-14 15:19:02 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def student():
+def index():
+    return render_template("index.html")
+
+@app.route('/single')
+def single_post():
     return render_template("single_post.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
